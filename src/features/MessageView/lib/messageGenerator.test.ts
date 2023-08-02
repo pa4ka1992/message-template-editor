0c808786be7ff8868564182d26a252b1b928955c
pa4ka1992 <sour945@gmail.com>
2023-08-02 20:43:19 +0300

    feat: add new tests for message generator

--- src/features/MessageView/lib/messageGenerator.test.ts
+++ src/features/MessageView/lib/messageGenerator.test.ts
@@ -1,26 +1,16 @@
-import { BLOCK_NAME, ConditionObj, ITemplateBlock } from 'shared';
+import { ITemplateBlock } from 'shared';
 import { messageGenerator, VarsObj } from './messageGenerator';
+import MOCKS from './mocks';
 
-const VARS = {
-  firstname: 'John',
-  lastname: 'Doe',
-  company: 'Burger King',
-  position: 'cook'
-};
-
-const TEMPLATE: ITemplateBlock = {
-  name: BLOCK_NAME.head,
-  value: '',
-  children: []
-};
+const { VARS, TEMPLATE, getCondition1, getCondition2, getCondition3 } = MOCKS;
 
 describe('message generator', () => {
   let template: ITemplateBlock;
   let vars: VarsObj;
 
   beforeEach(() => {
-    vars = VARS;
-    template = TEMPLATE;
+    vars = { ...VARS };
+    template = { ...TEMPLATE };
   });
 
   test('parses message without conditions', () => {
@@ -30,57 +20,56 @@ describe('message generator', () => {
     expect(messageGenerator(vars, template)).toEqual("Hello, I'm John Doe! I'm working at Burger King.");
   });
 
-  describe('parses message with conditions', () => {
-    test('with the one level deep', () => {
-      template.value = 'Hello, ';
+  test('parses message with one level deep conditions', () => {
+    template.value = 'Hello, ';
 
-      const condition1 = new ConditionObj('Nice to meet you. ');
-      const [ifBlock1, thenBlock1, elseBlock1] = condition1.blocks;
-      ifBlock1.value = '{ FIRSTNAME }';
-      thenBlock1.value = "I'm { FIRSTNAME }! ";
-      elseBlock1.value = 'you can call me Slave. ';
+    template.children = [getCondition1(), getCondition2(), getCondition3()];
 
-      const condition2 = new ConditionObj("So that's it. ");
-      const [ifBlock2, thenBlock2, elseBlock2] = condition2.blocks;
-      ifBlock2.value = '{ LASTNAME }';
-      thenBlock2.value = 'My second name is { LASTNAME }. ';
-      elseBlock2.value = 'Nobody cares about my second name, right? ';
+    expect(messageGenerator(vars, template)).toEqual(
+      "Hello, I'm John! Nice to meet you. My second name is Doe. So that's it. I'm working at Burger King. It has been going on for almost two years."
+    );
 
-      const condition3 = new ConditionObj('It has been going on for almost two years.');
-      const [ifBlock3, thenBlock3, elseBlock3] = condition3.blocks;
-      ifBlock3.value = '{ COMPANY }';
-      thenBlock3.value = "I'm working at { COMPANY }. ";
-      elseBlock3.value = 'I am unemployed. ';
+    vars.firstname = '';
+    vars.lastname = '';
+    vars.company = '';
 
-      template.children = [condition1, condition2, condition3];
+    expect(messageGenerator(vars, template)).toEqual(
+      "Hello, you can call me Slave. Nice to meet you. Nobody cares about my second name, right? So that's it. I am unemployed. It has been going on for almost two years."
+    );
+  });
 
-      expect(messageGenerator(vars, template)).toEqual(
-        "Hello, I'm John! Nice to meet you. My second name is Doe. So that's it. I'm working at Burger King. It has been going on for almost two years."
-      );
+  test('parses message with multiple level deep conditions', () => {
+    template.value = 'Hello, ';
 
-      vars.firstname = '';
-      vars.lastname = '';
-      vars.company = '';
+    const condition1 = getCondition1();
+    const condition2 = getCondition2();
 
-      expect(messageGenerator(vars, template)).toEqual(
-        "Hello, you can call me Slave. Nice to meet you. Nobody cares about my second name, right? So that's it. I am unemployed. It has been going on for almost two years."
-      );
-    });
+    condition2.blocks[1].children = [getCondition3()];
+    condition1.blocks[1].children = [condition2];
+    template.children = [condition1];
 
-    // test('with the multiple level deep', () => {
-    //   VARS.firstname = 'John';
-    //   VARS.lastname = 'Doe';
-    //   VARS.company = 'Burger King';
+    expect(messageGenerator(vars, template)).toEqual(
+      "Hello, I'm John! My second name is Doe. I'm working at Burger King. It has been going on for almost two years. So that's it. Nice to meet you."
+    );
+  });
 
-    //   TEMPLATE.value = "Hello, I'm { FIRSTNAME } { LASTNAME }! I'm working at { COMPANY }.";
-    //   // TEMPLATE.split = ' Bye';
-    //   // TEMPLATE.children[0].blocks[0].value = '{ COMPANY }';
-    //   // TEMPLATE.children[0].blocks[1].value = 'You are working at { COMPANY }.';
-    //   // TEMPLATE.children[0].blocks[2].value = 'Where are you working?';
+  test('parses message with multiple level deep conditions 2', () => {
+    template.value = 'Hello, ';
 
-    //   const message = messageGenerator(VARS, TEMPLATE);
+    const condition1 = getCondition1();
 
-    //   expect(message).toEqual("Hello, I'm John Doe! I'm working at Burger King.");
-    // });
+    condition1.blocks[2].children = [getCondition3()];
+    condition1.blocks[1].children = [getCondition2()];
+    template.children = [condition1];
+
+    expect(messageGenerator(vars, template)).toEqual(
+      "Hello, I'm John! My second name is Doe. So that's it. Nice to meet you."
+    );
+
+    vars.firstname = '';
+
+    expect(messageGenerator(vars, template)).toEqual(
+      "Hello, you can call me Slave. I'm working at Burger King. It has been going on for almost two years. Nice to meet you."
+    );
   });
 });
